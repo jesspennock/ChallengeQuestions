ec20f1402ef98af014df5e47a2ce0435f9245a53
Jess Pennock <jess.pennock@gmail.com>
2022-10-16 12:07:55 -0600

    Adding AddtoZero

---
+++ challenge-add-to-zero/addToZero.js
@@ -0,0 +1,28 @@
+
+// Given an array of numbers, console.log true if any two numbers in the array add to zero, otherwise log false.
+
+// For example:
+
+// [1, 4, 11, 2, 37, -4] should log true because 4 and -4 add to 0.
+
+// [0, 21, 33, 6, 0, -9] should log true because 0 and 0 add to 0.
+
+// [0, 1, 2, 3, 4, 5] should log false because no two numbers add to 0.
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+
+const addToZero = (arr) => {
+    let equalsZero = false
+    for(i=0; i<arr.length; i++){
+        for(j=0; j<arr.length; j++){
+
+            if(arr[i] + arr[j] === 0){
+                equalsZero = true
+            }
+        }
+    }
+    console.log(equalsZero)
+}
+addToZero(array)
